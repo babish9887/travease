e4b8b1bd4843c9c5459f12c47502c1103b2832f2
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-08-10 10:18:56 +0545

    add navbar

---
+++ src/app/(main)/layout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainLayout = ({ children }) => {
+  return <main className="px-6">{children}</main>;
+};
+
+export default MainLayout;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,104 @@
+"use client";
+import Link from "next/link";
+import React, { useRef, useState } from "react";
+import { Input } from "./ui/input";
+import { Menu } from "lucide-react";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  const ref = useRef(null);
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+    <nav className="w-full flex justify-between items-center py-5 rounded-3xl px-4 sticky top-0 bg-main-background">
+      <div>
+        <h2 className="font-extrabold lg:text-3xl text-2xl text-orange uppercase">
+          Travease
+        </h2>
+      </div>
+      <div className="md:flex items-center gap-8 lg:gap-12 font-semibold hidden text-base">
+        <Link href={"#"} className="hover:text-orange">
+          Home
+        </Link>
+        <Link href={"#"} className="hover:text-orange">
+          Our offer
+        </Link>
+        <Link href={"#"} className="hover:text-orange">
+          Booking
+        </Link>
+        <Link href={"#"} className="hover:text-orange">
+          FAQ
+        </Link>
+      </div>
+
+      {/* <div className="flex">
+        <Input
+          type="text"
+          placeholder="Search..."
+          className="rounded-full placeholder-black"
+        />
+      </div> */}
+
+      <div className="flex md:hidden flex-col items-end" ref={ref}>
+        <Menu
+          onClick={() => setShowMenu((value) => !value)}
+          className="h-10 w-10 rounded-full p-2 hover:[#DFD9CE] transition"
+        />
+        {showMenu ? (
+          <div className="absolute flex-col flex mt-10 bg-white px-3 py-4 rounded-lg text-sm gap-1 w-2/5 text-center">
+            <Link
+              href={"#"}
+              className=" hover:bg-orange hover:text-white rounded-md px-2 py-1 w-full transition"
+            >
+              Home
+            </Link>
+            <Link
+              className=" hover:bg-orange hover:text-white rounded-md px-2 py-1 w-full transition"
+              href={"#"}
+            >
+              Our offer
+            </Link>
+            <Link
+              className=" hover:bg-orange hover:text-white rounded-md px-2 py-1 w-full transition"
+              href={"#"}
+            >
+              Booking
+            </Link>
+            <Link
+              className=" hover:bg-orange hover:text-white rounded-md px-2 py-1 w-full transition"
+              href={"#"}
+            >
+              FAQ
+            </Link>
+
+            <div className="flex flex-col gap-2">
+              <Button
+                className="bg-orange text-white hover:bg-orange/80"
+                variant="secondary"
+                size="sm"
+              >
+                Sign in
+              </Button>
+              <Button className="text-black" variant="ghost" size="sm">
+                Sign up
+              </Button>
+            </div>
+          </div>
+        ) : null}
+      </div>
+
+      <div className="md:flex gap-4 hidden">
+      <Button
+                className="bg-orange text-white hover:bg-orange/80"
+                variant="secondary"
+              >
+                Sign in
+              </Button>
+              <Button className="text-black" variant="ghost" >
+                Sign up
+              </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
